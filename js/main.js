93d06e527c8236db4b1fb69966f4217bbb70f4ba
ksenya-torkova <ksenya.torkova@gmail.com>
2018-07-30 18:58:13 +0300

    laptop

--- js/main.js
+++ js/main.js
@@ -14,7 +14,6 @@ if (mainNavigationToggle) {
 }
 
 // main navigation sublist show
-
 var mainNavSubmenus = document.querySelectorAll('.nav__link--submenu');
 
 if (mainNavSubmenus) {
@@ -28,6 +27,60 @@ if (mainNavSubmenus) {
       var mainNavSubmenusIndex = evt.currentTarget.getAttribute('data-index');
       this.classList.toggle('nav__link--open');
       mainNavSublists[mainNavSubmenusIndex].classList.toggle('nav__sublist--visible');
+    });
+  }
+}
+
+// filter list show
+var mainFilterToggle = document.querySelector('.filter__toggle');
+
+if (mainFilterToggle) {
+  var mainFilterList = document.querySelector('.filter__list');
+  
+  mainFilterToggle.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    mainFilterToggle.classList.toggle('filter__toggle--active');
+    mainFilterList.classList.toggle('filter__list--visible');
+  });
+}
+
+// main filters open
+var mainFilterToggles = document.querySelectorAll('.filter__type');
+
+if (mainFilterToggles) {
+  var mainFilters = document.querySelectorAll('.filter__type-list');
+  
+  for (var i = 0; i < mainFilterToggles.length; i++) {
+    mainFilterToggles[i].setAttribute('data-index', i);
+    
+    mainFilterToggles[i].addEventListener('click', function (evt) {
+      evt.preventDefault();
+      var mainFiltersIndex = evt.currentTarget.getAttribute('data-index');
+      this.classList.toggle('filter__type--active');
+      mainFilters[mainFiltersIndex].classList.toggle('filter__type-list--visible');
+    });
+  }
+}
+
+// sorting list show ang toggle
+var mainSortingToggle = document.querySelector('.sorting__toggle');
+
+if (mainSortingToggle) {
+  var mainSortingList = document.querySelector('.sorting__options');
+  var mainSortingOptions = mainSortingList.querySelectorAll('.sorting__type');
+  
+  mainSortingToggle.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    mainSortingToggle.classList.toggle('sorting__toggle--active');
+    mainSortingList.classList.toggle('sorting__options--visible');
+  });
+  
+  for (var i = 0; i < mainSortingOptions.length; i++) {
+    mainSortingOptions[i].addEventListener('click', function (evt) {
+      evt.preventDefault();
+      var text = this.textContent;
+      console.log(text);
+      mainSortingToggle.textContent = text;
     });
   }
 }
